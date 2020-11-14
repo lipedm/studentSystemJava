b6d7c561685019d002271da2c3e6d3a1e7285d67
Felipe Fernandes <lipedm.fernandes@hotmail.com>
2020-11-13 21:47:51 -0300

    fix  environment

---
+++ src/main/teste.java
@@ -0,0 +1,18 @@
+package main;
+
+public class teste {
+
+    private String teste;
+
+    public String getTeste() {
+        return this.teste;
+    }
+
+    public void setTeste(String teste) {
+        this.teste = teste;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("teste");
+    }
+}
